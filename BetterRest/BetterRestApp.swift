a5509d898a1bb44bcbfb8da25b651b877a560ce6
guntherhaas <guntherhaas.eng@gmail.com>
2024-06-17 13:44:46 +0200

    initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Gunther Masi Haas on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
